facbd1174b6fff136434bdfc14b52425a238e403
Sudeep Joshi <89845072+sudeepj24@users.noreply.github.com>
2025-08-10 09:55:11 +0530

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -224,7 +224,7 @@ const HomePage: React.FC = () => {
         body: JSON.stringify({
           ...data,
           _subject: 'High Security Doors Ads - Contact Form Submission',
-          _cc: 'info@secure-house.co.uk'
+
         })
       });
       const result = await res.json();
